7aea22e0ab0e3f824805812853ee99f4a3029d3a
Vivek Singh <viksin672@gmail.com>
2019-08-17 03:22:35 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -11,7 +11,7 @@ class Cart extends Component {
       }
     
       componentDidMount(){
-        fetch("/data.json")
+        fetch("/data.js")
           .then(r => r.json())
           .then(data => {
               console.log(data);
@@ -22,46 +22,48 @@ class Cart extends Component {
 render(){
   
     if(this.state[0] != undefined){
-        return(
-            <div >
-               <div className="container">
-               <Cartext
-        activationfee= {this.state[0].activationfee}
-        activationfeetext= {this.state[0].activationfeetext}
-        capacity={this.state[0].capacity}
-        colorname={this.state[0].colorname}
-        contractterm={this.state[0].contractterm}
-        deliverymethod={this.state[0].deliverymethod}
-        deviceimageUrl={this.state[0].deviceimageUrl}
-        deviceimageurllarge={this.state[0].deviceimageurllarge}
-        devicemanufacturename={this.state[0].devicemanufacturename}
-        devicenickname={this.state[0].devicenickname}
-        devicenrodid={this.state[0].devicenrodid}
-        deviceproductdisplayname= {this.state[0].deviceproductdisplayname}
-        devicesorid={this.state[0].devicenrodid}
-        devicetype={this.state[0].devicetype}
-        discountededgeretailprice={this.state[0].discountededgeretailprice}
-        edgeitemdownpaymentamount={this.state[0].edgeitemdownpaymentamount}
-        editdeviceurl={this.state[0].editdeviceurl}
-        fullretailprice={this.state[0].fullretailprice}
-        installmentbalance={this.state[0].installmentbalance}
-        inventorystatus={this.state[0].inventorystatus}
-        lacprice={this.state[0].lacprice}
-        lactext={this.state[0].lactext}
-        loanterm={this.state[0].loanterm}
-        monthlypriceafterdownpaymentbeforediscount={this.state[0].monthlypriceafterdownpaymentbeforediscount}
-        originalprice={this.state[0].originalprice}
-        price={this.state[0].price}
-        priceforfullretailpricelistid={this.state[0].priceforfullretailpricelistid}
-        pricesubtitle={this.state[0].pricesubtitle}
-        pricetext={this.state[0].pricetext}
+        var rows = [];
+        var i;
+for (i in this.state) {
+    rows.push(<Cartext
+        activationfee= {this.state[i].activationfee}
+        activationfeetext= {this.state[i].activationfeetext}
+        capacity={this.state[i].capacity}
+        colorname={this.state[i].colorname}
+        contractterm={this.state[i].contractterm}
+        deliverymethod={this.state[i].deliverymethod}
+        deviceimageUrl={this.state[i].deviceimageUrl}
+        deviceimageurllarge={this.state[i].deviceimageurllarge}
+        devicemanufacturename={this.state[i].devicemanufacturename}
+        devicenickname={this.state[i].devicenickname}
+        devicenrodid={this.state[i].devicenrodid}
+        deviceproductdisplayname= {this.state[i].deviceproductdisplayname}
+        devicesorid={this.state[i].devicenrodid}
+        devicetype={this.state[i].devicetype}
+        discountededgeretailprice={this.state[i].discountededgeretailprice}
+        edgeitemdownpaymentamount={this.state[i].edgeitemdownpaymentamount}
+        editdeviceurl={this.state[i].editdeviceurl}
+        fullretailprice={this.state[i].fullretailprice}
+        installmentbalance={this.state[i].installmentbalance}
+        inventorystatus={this.state[i].inventorystatus}
+        lacprice={this.state[i].lacprice}
+        lactext={this.state[i].lactext}
+        loanterm={this.state[i].loanterm}
+        monthlypriceafterdownpaymentbeforediscount={this.state[i].monthlypriceafterdownpaymentbeforediscount}
+        originalprice={this.state[i].originalprice}
+        price={this.state[i].price}
+        priceforfullretailpricelistid={this.state[i].priceforfullretailpricelistid}
+        pricesubtitle={this.state[i].pricesubtitle}
+        pricetext={this.state[i].pricetext}
         
-        />
-                 </div>
-             </div>  
-       );
-       
+        />);
     }
+     return <tbody>
+         <div className="container">
+         {rows}
+         </div>
+     </tbody>;
+         }
     return(
     <div>
         <h1>Loading..</h1>

6b27d6333afbd399913d57f5bf75be6c2b24acb8
Vivek Singh <viksin672@gmail.com>
2019-08-17 02:16:51 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,72 +1,74 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Cart.css"
+import "./Cart.css";
+import Cartext from "./Cartext";
+
 
 
 class Cart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
+    
+      componentDidMount(){
+        fetch("/data.json")
+          .then(r => r.json())
+          .then(data => {
+              console.log(data);
+            this.setState(data);
+          })
+      };
+      
 render(){
+  
+    if(this.state[0] != undefined){
+        return(
+            <div >
+               <div className="container">
+               <Cartext
+        activationfee= {this.state[0].activationfee}
+        activationfeetext= {this.state[0].activationfeetext}
+        capacity={this.state[0].capacity}
+        colorname={this.state[0].colorname}
+        contractterm={this.state[0].contractterm}
+        deliverymethod={this.state[0].deliverymethod}
+        deviceimageUrl={this.state[0].deviceimageUrl}
+        deviceimageurllarge={this.state[0].deviceimageurllarge}
+        devicemanufacturename={this.state[0].devicemanufacturename}
+        devicenickname={this.state[0].devicenickname}
+        devicenrodid={this.state[0].devicenrodid}
+        deviceproductdisplayname= {this.state[0].deviceproductdisplayname}
+        devicesorid={this.state[0].devicenrodid}
+        devicetype={this.state[0].devicetype}
+        discountededgeretailprice={this.state[0].discountededgeretailprice}
+        edgeitemdownpaymentamount={this.state[0].edgeitemdownpaymentamount}
+        editdeviceurl={this.state[0].editdeviceurl}
+        fullretailprice={this.state[0].fullretailprice}
+        installmentbalance={this.state[0].installmentbalance}
+        inventorystatus={this.state[0].inventorystatus}
+        lacprice={this.state[0].lacprice}
+        lactext={this.state[0].lactext}
+        loanterm={this.state[0].loanterm}
+        monthlypriceafterdownpaymentbeforediscount={this.state[0].monthlypriceafterdownpaymentbeforediscount}
+        originalprice={this.state[0].originalprice}
+        price={this.state[0].price}
+        priceforfullretailpricelistid={this.state[0].priceforfullretailpricelistid}
+        pricesubtitle={this.state[0].pricesubtitle}
+        pricetext={this.state[0].pricetext}
+        
+        />
+                 </div>
+             </div>  
+       );
+       
+    }
     return(
-         <div >
-             <div className="d1">
-             <h3 className="hed1"> Faster & save online <span className="di1">|</span> <span className="hed2"> Get it fast with In-Store Pickup or Same Day Delivery, plus reduced activation fees.
-             <i class="fas fa-info-circle"></i></span> </h3>
-             </div>
-            <div className="container">
-                  <h2 className="t1">Look's like your cart is empty. Take it on a shopping spree</h2>
-                  <div className="row con">
-                   <div className="col-sm-6">
-                       <hr/>
-                       <h1>$0.00</h1>
-                       <p>Due today</p>
-                   </div>
-                   <div className="col-sm-6">
-                       <hr/>
-                       <h1>$0.00</h1>
-                       <p>Due Monthly</p>
-                   </div>
-                  
-                   <div className="col-sm-12">
-                   <hr/>
-                   </div>
-                   <div className="col-sm-6">
-                       <h3>Need to return or exchange a purchase? No problem.</h3>
-                       
-                   </div>
-                   <div className="col-sm-6">
-                       <h3>Purchase with confidence with our holiday return policy.</h3>
-                       <p>A restocking fee of $50 applies to any return or exchange of a wireless device (excluding Hawaii).</p>
-                   </div>
-                   <div className="col-sm-12">
-                   <hr/>
-                   </div>
-                   <div className="col-sm-6">
-                       <h3>How soon could your order be ready?</h3>
-                   </div>
-                   <div className="col-sm-6">
-                       <h3>Free in-store pickup</h3>
-                       <p>You can pick up your order today at a store near you.</p>
-                       <h4>or get your order with free 2-day shipping.</h4>
-                   </div>
-                   <div className="col-sm-12">
-                   <hr/>
-                   </div>
-                   <Link to="/prodlist" type="button" class="btn btn-dark btn-lg b1">Add a line</Link>
-                   <div className="col-sm-12">
-                   <hr/>
-                   </div>
-                   </div>
-                  <h6>Want to retrieve a saved cart?</h6>
-                   <p className="sm1">Enter your email address*</p>
-                    <div>
-                        <input></input>
-                        <span className="b2">
-                        <button type="button" class="btn btn-dark btn-lg b1">        Go        </button>
-                        </span>
-                    </div>
-            </div>
-          </div>  
-    )
+    <div>
+        <h1>Loading..</h1>
+    </div>
+    );
+    }
 }
-}
+
 
 export default Cart;
